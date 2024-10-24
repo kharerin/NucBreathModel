53f4d43083746204096badd337b4b3d03ff5cd7b
kharerin <kharerin@iitmandi.ac.in>
2024-10-24 20:57:40 +0530

    Add files via upload

---
+++ lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d_amin1/mean_dg_vs_mu.py
@@ -0,0 +1,21 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import pandas as pd
+import os 
+
+with open('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d_amin1/mean_dg_vs_mu/d1_mu_E0d.txt', 'w') as newfile:
+     for i in range(6):
+         x=i+1
+         #fx=''.join((('/nucdir%d'% x),'/d1_mu_E0d.txt'))
+         print(x)
+         with open(os.path.join(('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d_amin1/mean_dg_vs_mu/nucdir%d/'% x), 'd1_mu_E0d.txt')) as f: 
+              contents = f.read()
+              newfile.write(contents)
+  
+ 
+
+
+
+
