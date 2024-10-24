41c9fef306788898e2753c4d628d199dcf843d15
kharerin <kharerin@iitmandi.ac.in>
2024-10-24 12:01:26 +0530

    Add files via upload

--- Figure_1/Figure_1_mnase200U.py
+++ Figure_1/Figure_1_mnase200U.py
@@ -17,33 +17,33 @@ o11 = np.zeros(Ns)
 for i in range(Ns):
     o11[i]=amin+i
 
-f = open('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/yeastsc_lnZ/H4_Input_MNase_200U_Rep1/frag_91_147bp_gene.dat','r')
+f1='/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/yeastsc_lnZ/H4_Input_MNase_200U_Rep1/'
+f2='frag_91_147bp_gene.dat'
+fx=''.join((f1,f2))
+print('filename...',fx)
+f = open(fx,'r')
 for row in f:
     row = row.split(' ')
     o2.append(float(row[0]))
 f.close()
 
-f = open('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/yeastsc_lnZ/H4_Input_MNase_200U_Rep1/frag_91_147bp_nong.dat','r')
+f2='frag_91_147bp_nong.dat'
+fx=''.join((f1,f2))
+print('filename...',fx)
+f = open(fx,'r')
 for row in f:
     row = row.split(' ')
     o3.append(float(row[0]))
 f.close()
 
-i=0
-sumo1=0
-f = open('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/yeastsc_lnZ/H4_Input_MNase_200U_Rep1/frag_91_147bp.dat','r')
+f2='frag_91_147bp.dat'
+fx=''.join((f1,f2))
+print('filename...',fx)
+f = open(fx,'r')
 for row in f:
     row = row.split(' ')
     o1.append(float(row[0]))
-    sumo1=sumo1+o1[i];
 f.close()
-print(sumo1)
-
-#f = open('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/mu_-21p6kt_E0d8p4/nieb2/kevin_fig_ver2/large_remod2/score_scanA1/basic_nieb0_E0d_-3p08_mu_-10p5/frag_nie2_occ_0pxx.txt','r')
-#for row in f:
-#    row = row.split(' ')
-#    o4.append(float(row[0]))
-#f.close()
 
 ax2=plt.subplot(1, 2, 1)
 ax2.text(-0.18, 1.03, string.ascii_uppercase[0], transform=ax2.transAxes, size=20, weight='bold')
@@ -60,25 +60,7 @@ plt.plot(o11, o3, c = 'c')
 #plt.plot(o11, o4, c = 'r')
 
 plt.legend(('All', 'Gene', 'Non-gene')) #200U
-
-
-#plt.xticks(np.arange(0, 400, 40))
-#plt.yticks(np.arange(0, 1, 0.1))
 plt.grid()
-#plt.vlines(0, 0, 0.015, linestyles ="dashed", colors ="gray")
-#plt.vlines(-0.05, 0, 0.015, linestyles ="dashed", colors ="red")
-#plt.setp(ax2.get_yticklabels(), visible=False)
-
-#ax2=plt.subplot(2, 2, 1)
-#plt.setp(ax2.get_xticklabels(), visible=False)
-#plt.setp(ax2.get_yticklabels(), visible=False)
-#ax2.set_xticks([])
-#ax2.set_yticks([])
-#ax2=plt.subplot(2, 2, 3)
-#plt.setp(ax2.get_xticklabels(), visible=False)
-#plt.setp(ax2.get_yticklabels(), visible=False)
-#ax2.set_xticks([])
-#ax2.set_yticks([])
 
 
 data3=np.load('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/yeastsc_lnZ/H4_Input_MNase_200U_Rep1/pair_end/chrmap.npy')
@@ -106,8 +88,6 @@ cb=plt.colorbar(imx1,cax,shrink=0.4)
 #plt.setp(ax4.get_xticklabels(), visible=False)
 print(data5.max())
 print(data5.min())
-
-#data3=np.load('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/yeastsc_lnZ/H4_Input_MNase_200U_Rep1/pair_end/chrmap.npy')
 
 o1_200u=data5.sum(axis=0)
 data5x=data5[0:7,:]
