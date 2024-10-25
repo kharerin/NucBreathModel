e25d3b02ea4af12941359a549facad8e1814ca5e
kharerin <kharerin@iitmandi.ac.in>
2024-10-25 16:18:58 +0530

    Update mean_dg_vs_mu.py

--- lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu.py
+++ lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu.py
@@ -1,16 +1,14 @@
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import math
 import pandas as pd
 import os 
 
-with open('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu/d1_mu_E0d.txt', 'w') as newfile:
+with open('/lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu/d1_mu_E0d.txt', 'w') as newfile:
      for i in range(6):
          x=i+1
          #fx=''.join((('/nucdir%d'% x),'/d1_mu_E0d.txt'))
          print(x)
-         with open(os.path.join(('/media/hungyo/edrive/hung_ens_laptop/hungyo_nucdyn/NucleoModel/lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu/nucdir%d/'% x), 'd1_mu_E0d.txt')) as f: 
+         with open(os.path.join(('/lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu/nucdir%d/'% x), 'd1_mu_E0d.txt')) as f: 
               contents = f.read()
               newfile.write(contents)
   
