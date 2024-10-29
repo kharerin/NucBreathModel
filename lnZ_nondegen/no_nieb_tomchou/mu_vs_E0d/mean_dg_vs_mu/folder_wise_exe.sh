854786542b72ae2245a553f9aa7680221fe38bcf
kharerin <kharerin@iitmandi.ac.in>
2024-10-29 19:04:03 +0530

    Add files via upload

---
+++ lnZ_nondegen/no_nieb_tomchou/mu_vs_E0d/mean_dg_vs_mu/folder_wise_exe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {1..6}
+do
+  mkdir nucdir$i
+  cp hung_soft_lnZ_set.c ./nucdir$i/hung_soft_lnZ_set.c
+  cp jop_script.sh ./nucdir$i/jop_script.sh
+  cd nucdir$i
+  sed -i "s:replacepath:$(pwd):" hung_soft_lnZ_set.c
+  mv hung_soft_lnZ_set.c hung_soft_lnZ_set$i.c
+  sed -i "s/tn=1/tn=$i/" hung_soft_lnZ_set$i.c
+  sed -i "s/hung_soft_lnZ_set.c/hung_soft_lnZ_set$i.c/" jop_script.sh
+  sed -i "s/ms0p1x/coop0_$i/" jop_script.sh
+  sed -i "s/den.out/den$i.out/" jop_script.sh
+  sed -i "s/denout.txt/denout$i.txt/" jop_script.sh
+  ./jop_script.sh
+ cd ..
+done 
